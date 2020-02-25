e47cd1675f66de59ace11dd713c8cc34587d2ba2
Gijs Burghoorn <g.burghoorn@gmail.com>
2020-02-25 10:52:08 +0100

    Added a prelude with easy useful definitions

---
+++ src/prelude.rs
@@ -0,0 +1,5 @@
+//! Contains some useful definitions for SVG Graphics, also re-exports svg_definitions
+
+pub use crate::default as SVGDefault;
+pub use crate::renderer::Renderer as SVGRenderer;
+pub use svg_definitions::prelude::*;
