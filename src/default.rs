ac7eb617e740f87a058fba9ee419caa03d5f77b5
Gijs Burghoorn <g.burghoorn@gmail.com>
2020-02-25 10:48:40 +0100

    Amended some documentation for default

--- src/default.rs
+++ src/default.rs
@@ -1,5 +1,8 @@
+//! Contains some easy and nice ways to create definitions and shapes to render
+
 use svg_definitions::prelude::*;
 
+/// Creates a default circle with a certain radius
 pub fn circle(radius: i32) -> SVGElem {
     SVGElem::new(Tag::Circle)
         .set(Attr::Radius, radius.into())
@@ -8,6 +11,7 @@ pub fn circle(radius: i32) -> SVGElem {
         .set(Attr::FillColor, RGBT::Transparent.into())
 }
 
+/// Creates a default rectangle with a certain width and height
 pub fn rect(width: i32, height: i32) -> SVGElem {
     SVGElem::new(Tag::Rectangle)
         .set(Attr::Width, width.into())
@@ -17,6 +21,7 @@ pub fn rect(width: i32, height: i32) -> SVGElem {
         .set(Attr::FillColor, RGBT::Transparent.into())
 }
 
+/// Creates a default curve with control points 1 and 2 and an end point
 pub fn curve(x: i32, y: i32, cx1: i32, cy1: i32, cx2: i32, cy2: i32) -> SVGElem {
     SVGElem::new(Tag::SVGPath).set(
         Attr::PathDefinition,
@@ -31,6 +36,7 @@ pub fn curve(x: i32, y: i32, cx1: i32, cy1: i32, cx2: i32, cy2: i32) -> SVGElem 
     )
 }
 
+/// Creates a polygon from a vector of points
 pub fn polygon(points: Vec<(i32, i32)>) -> SVGElem {
     let mut path_string = PathString::new().move_to(as_point_2d(points[0]));
 
@@ -38,7 +44,10 @@ pub fn polygon(points: Vec<(i32, i32)>) -> SVGElem {
         .iter()
         .for_each(|point| path_string = path_string.clone().line_to(as_point_2d(*point)));
 
-    SVGElem::new(Tag::SVGPath).set(Attr::PathDefinition, path_string.into())
+    SVGElem::new(Tag::SVGPath)
+        .set(Attr::PathDefinition, path_string.into())
+        .set(Attr::StrokeWidth, 1.into())
+        .set(Attr::StrokeColor, RGB::new(0, 0, 0).into())
 }
 
 fn as_point_2d(point: (i32, i32)) -> Point2D {
